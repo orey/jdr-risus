ca58b3722f0a11a4d023ab63e255c1fcd9fa4b2f
orey <rey.olivier@gmail.com>
2022-05-07 18:11:37 +0200

    new stuff

--- rel.sh
+++ rel.sh
@@ -2,10 +2,9 @@
 clear
 
 release () {
-    echo ${files} $origin
-    for f in ${files}
+    for f in ${files[@]}
     do
-        echo $f
+        echo "=> $f"
         if [ -e ./$origin/$f ]; 
         then
             mv ./$origin/$f ./_release
@@ -15,17 +14,21 @@ release () {
         fi
     done
 }
-
+# Declare an array of string with type
 origin=flowchart
-files=(risus-flowchart.pdf risus-flowchart.png risus-flowchart-big.png)
+declare -a files=("risus-flowchart.pdf" "risus-flowchart.png" "risus-flowchart-big.png")
 release
 
 origin=ecran
-files=(Risus-Ecran-A3-OReyJdr05.pdf)
+declare -a files=("Risus-Ecran-A3-OReyJdr05.pdf")
 release
 
 origin=ecran_fd
-files=(Risus-GM-Screen-fr-OReyJdr09.pdf)
+declare -a files=("Risus-GM-Screen-fr-OReyJdr09.pdf")
+release
+
+origin=risus-mega
+declare -a files=("Risus-Mega-OReyJdr07.pdf")
 release
 
 echo "Done"
